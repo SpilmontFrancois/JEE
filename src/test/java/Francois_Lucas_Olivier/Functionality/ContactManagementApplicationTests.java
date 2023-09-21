470c3a8481ae9322636da412276cf24a2147b7ba
Mordreckk <mathoulolivier@gmail.com>
2023-09-21 08:48:39 +0200

    ok

---
+++ src/test/java/Francois_Lucas_Olivier/Functionality/ContactManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package Francois_Lucas_Olivier.Functionality;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
