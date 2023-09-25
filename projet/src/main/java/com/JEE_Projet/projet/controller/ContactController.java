0a5d2630d3b29902d6ac5dc7e269ca5bbebdfbca
SpilmontFrancois <francois.spilmont5@etu.univ-lorraine.fr>
2023-09-25 13:47:18 +0200

    feat: add swagger doc & handle not founds

--- projet/src/main/java/com/JEE_Projet/projet/controller/ContactController.java
+++ projet/src/main/java/com/JEE_Projet/projet/controller/ContactController.java
@@ -3,6 +3,11 @@ package com.JEE_Projet.projet.controller;
 import com.JEE_Projet.projet.model.Contact;
 import com.JEE_Projet.projet.response.ResponseHandler;
 import com.JEE_Projet.projet.service.ContactService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,32 +16,17 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/contacts")
+@RequestMapping("/api/contacts")
 @AllArgsConstructor
 public class ContactController {
     private final ContactService contactService;
 
-    @PostMapping("")
-    public ResponseEntity<Object> create(@RequestBody Contact contact) {
-        try {
-            Contact result = contactService.create(contact);
-            return ResponseHandler.generateResponse("Successfully created contact", HttpStatus.CREATED, result);
-        } catch (Exception e) {
-            return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.MULTI_STATUS, null);
-        }
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Object> delete(@PathVariable Integer id) {
-        try {
-            String result = contactService.delete(id);
-            return ResponseHandler.generateResponse("Successfully deleted contact", HttpStatus.NO_CONTENT, result);
-        } catch (Exception e) {
-            return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.MULTI_STATUS, null);
-        }
-    }
-
     @GetMapping("")
+    @RequestMapping(value = "", method = RequestMethod.GET, produces = "application/json")
+    @Operation(summary = "Get all contacts")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the contacts")
+    })
     public ResponseEntity<Object> getAll() {
         try {
             List<Contact> result = contactService.getAll();
@@ -46,12 +36,58 @@ public class ContactController {
         }
     }
 
-    //route for fetch one contact
     @GetMapping("/{id}")
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = "application/json")
+    @Operation(summary = "Get a contact by its id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the contact", content =  @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "404", description = "Contact not found", content = @Content)
+    })
     public ResponseEntity<Object> getOne(@PathVariable Integer id) {
         try {
             Contact result = contactService.getOne(id);
+
+            if (result == null)
+                return ResponseHandler.generateResponse("Contact not found", HttpStatus.NOT_FOUND, null);
+
             return ResponseHandler.generateResponse("Successfully fetched contact", HttpStatus.OK, result);
+        } catch (Exception e) {
+            return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.MULTI_STATUS, null);
+        }
+    }
+
+    @PostMapping("")
+    @RequestMapping(value = "", method = RequestMethod.POST, produces = "application/json")
+    @Operation(summary = "Create a new contact")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Successfully created the contact", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Contact.class))),
+            @ApiResponse(responseCode = "422", description = "Invalid input", content = @Content)
+    })
+    public ResponseEntity<Object> create(@RequestBody Contact contact) {
+        try {
+            Contact result = contactService.create(contact);
+
+            return ResponseHandler.generateResponse("Successfully created contact", HttpStatus.CREATED, result);
+        } catch (Exception e) {
+            return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.MULTI_STATUS, null);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE, produces = "application/json")
+    @Operation(summary = "Delete a contact by its id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Successfully deleted the contact", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "404", description = "Contact not found", content = @Content)
+    })
+    public ResponseEntity<Object> delete(@PathVariable Integer id) {
+        try {
+            String result = contactService.delete(id);
+
+            if (result == null)
+                return ResponseHandler.generateResponse("Contact not found", HttpStatus.NOT_FOUND, null);
+
+            return ResponseHandler.generateResponse("Successfully deleted contact", HttpStatus.NO_CONTENT, result);
         } catch (Exception e) {
             return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.MULTI_STATUS, null);
         }
