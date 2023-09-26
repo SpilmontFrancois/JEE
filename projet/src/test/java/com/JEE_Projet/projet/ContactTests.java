00c82c38faf51e89b515bbcbcfd0f77699516e53
Mordreckk <mathoulolivier@gmail.com>
2023-09-26 11:46:39 +0200

    Add test

---
+++ projet/src/test/java/com/JEE_Projet/projet/ContactTests.java
@@ -0,0 +1,35 @@
+package com.JEE_Projet.projet;
+import com.JEE_Projet.projet.model.Contact;
+import com.JEE_Projet.projet.repository.ContactRepository;
+import com.JEE_Projet.projet.service.ContactService;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest(classes = ContactRepository.class)
+
+
+public class ContactTests {
+
+	@Mock
+	private ContactRepository contactRepository;
+
+	@InjectMocks
+	private ContactService contactService;
+
+
+	@Test
+	void testget(){
+
+		Contact cTest= new Contact(1,"Spill","François","MALE","01/01/2000","lulu@home.fr","06060606","Français","France");
+		//then
+		assertNotNull(cTest);
+		assertEquals(1, cTest.getId());
+
+	}
+
+}
